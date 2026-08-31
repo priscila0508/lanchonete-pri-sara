21325580e838fdedb4885ffff8cfa9c8c8069088
EA - Priscila Andrade Gonçalves Pereira <priscila_andrade@educadventista.org>
2026-08-31 18:22:53 -0300

    Fim

--- script.js
+++ script.js
@@ -1,84 +1,51 @@
 const button = document.querySelector("button")
-const codigo = document.getElementById("codigo")
-const quantidade = document.getElementById("quantidade")
+const inputCodigo = document.getElementById("codigo")
+const inputQuantidade = document.getElementById("quantidade")
 const paragrafoResultado = document.getElementById("resultado")
 
 button.onclick = calcularValor
 
 function calcularValor() {
-    const ValorProduto = codigo.valueAsNumber
-    const quantidadeComprada = quantidade.valueAsNumber
+    const codigo = inputCodigo.valueAsNumber
+    const quantidade = inputQuantidade.valueAsNumber
 
-    if (codigo.value === "") {
+    if (inputCodigo.value === "" || inputQuantidade.value === "") {
         alert("Digite um valor válido.")
         return
     }
 
-    if(codigo.value === 1){
-        const produto1 = codigo.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+    if (codigo === 1) {
+        const total = quantidade * 5
+        const produto1 = total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 
-        paragrafoResultado.textContent = `
-            Valor a pagar:  ${produto1}
-        `
-
+        paragrafoResultado.textContent = `Valor a pagar:  ${produto1}`
     }
 
-    else if(codigo.value === 2){
+    else if (codigo === 2) {
+        const total = quantidade * 3.50
+        const produto2 = total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 
-        const produto2 = codigo.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-
-        paragrafoResultado.textContent = `
-            Valor a pagar:  ${produto2}
-        `
+        paragrafoResultado.textContent = `Valor a pagar:  ${produto2}`
     }
 
-    else if(codigo.value === 3){
+    else if (codigo === 3) {
+        const total = quantidade * 4.80
+        const produto3 = total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 
-        const produto3 = codigo.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-
-        paragrafoResultado.textContent = `
-            Valor a pagar:  ${produto3}
-        `
+        paragrafoResultado.textContent = `Valor a pagar:  ${produto3}`
     }
 
-    else if(codigo.value === 4){
+    else if (codigo === 4) {
+        const total = quantidade * 8.90
+        const produto4 = total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 
-        const produto4 = codigo.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-
-        paragrafoResultado.textContent = `
-            Valor a pagar:  ${produto4}
-        `
+        paragrafoResultado.textContent = `Valor a pagar:  ${produto4}`
     }
 
-    else if(codigo.value === 5){
+    else if (codigo === 5) {
+        const total = quantidade * 7.32
+        const produto5 = total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
 
-        const produto5 = codigo.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-
-        paragrafoResultado.textContent = `
-            Valor a pagar:  ${produto5}
-        `
+        paragrafoResultado.textContent = `Valor a pagar:  ${produto5}`
     }
-
-
-
-
-    if (dinheiroRecebido >= calculo) {
-        const troco = dinheiroRecebido -  calculo
-        const trocoConvertido = troco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-        paragrafoResultado.textContent = `
-            TROCO = ${trocoConvertido}
-        `
-    } else {
-        const faltando = calculo - dinheiroRecebido
-
-        const faltandoConvertido = faltando.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-
-        paragrafoResultado.textContent = `
-            DINHEIRO INSUFICIENTE, FALTAM ${faltandoConvertido}
-        `
-    }
-
-    preço.value = ""
-    quantidade.value = ""
-    valor.value = ""
 }
